e7a12b92b396e5c912c4fdda08659d8ab765f1af
hi-abhishek12 <abhisahni2005@gmail.com>
2025-06-08 11:21:58 +0000

    notes

--- notes/src/componets/store/todoSlice.js
+++ notes/src/componets/store/todoSlice.js
@@ -1,5 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice , nonoid} from '@reduxjs/toolkit'
+import { act } from 'react'
 
 const initialState = {
-   todos : []
+   notes : [{
+      id : 1,
+      notesTitle : '',
+      notesMsg : ''
+   }]
 }
+
+const notesSlice = createSlice({
+   name : 'notes',
+   initialState,
+   reducers : {
+      addNote : (state , action) =>{
+         const todo = {
+            id : nonoid(),
+            notesTitle : action.payload,
+            notesMsg : action.payload
+         }
+         state.notes.push(todo)
+      },
+      updateNote : (state , action) =>{
+         notes.map((note) => note.id === action.payload.id(
+           
+         ))
+      },
+      removeNote : (state , action) => {
+
+      }
+      
+   }
+})
+export const {addNote , updateNote , removeNote} = notesSlice.actions
+export default notesSlice.reducer
